d1c1e8fb07eeddd969cb7583a32e6e4de3863162
illuhich <1234papricka@gmail.com>
2019-04-24 21:55:23 +0300

    yeees

--- redkolis/Proj_for_3/proj_3/project/somesing.py
+++ redkolis/Proj_for_3/proj_3/project/somesing.py
@@ -1,3 +1,14 @@
 a = 5
 if a == 5:
     print('Yees')
+
+a = str(input())
+print(a[2])
+print(a[-2])
+print(a[0:5])
+print(a[0:-2])
+print(a[::2])
+print(a[1::2])
+print(a[::-1])
+print(a[::-2])
+print(len(a))
