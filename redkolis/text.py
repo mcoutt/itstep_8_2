d4b8f744ede80b210a0e76b709d9791485384f64
illuhich <1234papricka@gmail.com>
2019-04-26 01:50:18 +0300

    text

--- redkolis/text.py
+++ redkolis/text.py
@@ -2,7 +2,7 @@ from os import listdir
 
 
 def readfile():
-    file = open('text.txt', 'r')  # отображает содержимое в файле после изменения
+    file = open(choise, 'r')  # отображает содержимое в файле после изменения
     for line in file:
         print(line)
     file.close()
@@ -21,48 +21,50 @@ try:
         choise = input('Выберите один из них либо введите "new" для создания нового:\n') + '.txt'
         if choise == 'new.txt':
             name = input('Введите название нового файла: ') + '.txt'
-            file = open(name, 'a+')
-            file.write(input("Введите то что хотите записать в файл: "))  # создание и работа с новым файлом
-            file.write('\n')
-            file.close()
+            while 1:
+                if name == 'new.txt':
+                    name = input('Введите другое название для файла: ') + '.txt'
+                else:
+                    print('Файл с названием "{0}" успешно создан'.format(name.replace('.txt', '')))
+                    file = open(name, 'a+')
+                    file.write(' ')
+                    file.close()
+                    break
         elif choise in mytxt:
             print('Ключевые фразы:\n', '"write" или "запись" - запись с новой строки\n',
                   '"clear" или "очистить" - удалить всё и записать с начала\n',
-                  '"list" - для просмотра всех существующих файлов\n', '"close" - закрыть файл и выбрать другой\n',
+                  '"close" - закрыть файл и выбрать другой\n',
                   '"exit" или "выход" - завершение работы\n')
+            print("Вот содержимое файла:\n")
+            readfile()
             while TF:
-                action: str = str(input('Выберите действие с файлом: '))  # выбор действия в файле
-
-                if action == 'open' or action == 'открыть':
-                    print("Вот содержимое файла:\n")
-                    readfile()
-                elif action == 'close':
-                    break
-                elif action == 'exit' or action == 'выход':
-                    TF1 = False
-                    break
-                elif action == 'write' or action == 'запись':
-                    try:
-                        file = open('text.txt', 'a+')
+                try:
+                    action: str = str(input('Выберите действие с файлом: '))  # выбор действия в файле
+                    if action == 'close':
+                        break
+                    elif action == 'exit' or action == 'выход':
+                        TF1 = False
+                        break
+                    elif action == 'write' or action == 'запись':
+                        file = open(choise, 'a+')
                         file.write(input("Введите то что хотите записать в файл: "))  # записывает новую строку в файл
                         file.write('\n')
-                    except Exception as e:
-                        print(e)
-                    finally:
                         file.close()
                         print("Вот содержимое файла после изменений:\n")
                         readfile()
-                elif action == 'clear' or action == 'очистить':
-                    file = open('text.txt', 'w+')
-                    file.write(
-                        input("Введите то что хотите записать в файл: "))  # записывает новую строку в очищенный файл
-                    file.write('\n')
-                    file.close()
-                    print("Вот новое содержимое файла:\n")
-                    readfile()
-
-                else:
-                    print('Wrong')
+                    elif action == 'clear' or action == 'очистить':
+                        file = open(choise, 'w+')
+                        file.write(
+                            input("Введите то что хотите записать в файл: "))
+                        file.write('\n')  # записывает новую строку в очищенный файл
+                        file.close()
+                        print("Вот новое содержимое файла:\n")
+                        readfile()
+                    else:
+                        print('Wrong')
+                        continue
+                except Exception as e:
+                    print(e)
         else:
             print('Вводить можно только название существующего файла либо "new" для создания нового!')
 except Exception as ex:
