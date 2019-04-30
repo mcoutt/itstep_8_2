de3958c67c89b0b5ed2e66f101450f249edd7fe3
ArtemPD < >
2019-04-30 22:28:07 +0300

    some 3

--- APerehon/homework2++.py
+++ APerehon/homework2++.py
@@ -9,12 +9,12 @@ num = int(input("Введите число:"))
 
 
 if num > 0:
-    print ("Вы ввели число больше 0 ")
-elif int (num) < -10:
-    print ("Вы ввели число меньше -10")
+    print("Вы ввели число больше 0 ")
+elif int(num) < -10:
+    print("Вы ввели число меньше -10")
 else:
     print("Вы ввели число меньше 0 и больше -10")
-print ("All is okey")
+print("All is okey")
 
 
 
