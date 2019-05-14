b3fd177e40466516e25cf92aae2545f284f2ac7f
illuhich <1234papricka@gmail.com>
2019-05-14 15:56:27 +0300

    homework

--- redkolis/illya.py
+++
@@ -1,9 +0,0 @@
-class Human:
-    def __index__(self, name, age, language):
-        self.name = name
-        self.age = age
-        self.language = language
-
-    def speak(self):
-        return
-
