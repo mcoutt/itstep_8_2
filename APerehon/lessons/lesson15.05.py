de9b76db43dab21dbb294e974da1d576f7868d0b
ArtemPD <lenusenko@gmail.com>
2019-05-15 20:26:10 +0300

    ok

--- APerehon/lessons/lesson15.05.py
+++ APerehon/lessons/lesson15.05.py
@@ -1,1 +1,13 @@
-print('Hello')
+class Home:
+    def __init__(self,w,l,h = 125):
+        self.w = w
+        self.l = l
+        self.h = h
+
+    @classmethod
+    def net_speed(count):
+        return f'in the city internet speed  is: {count}'
+    def water_supply(self, hot=None):
+        return f'hot water is: {hot}'
+
+h = Home(w=10,l=15)
