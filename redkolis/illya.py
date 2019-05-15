a2c2b61ccc681c2284cf03c2eedfac5a0c572402
illuhich <1234papricka@gmail.com>
2019-05-15 14:57:55 +0300

    homework

--- redkolis/illya.py
+++ redkolis/illya.py
@@ -1,1 +1,11 @@
 print('1')
+t = True
+if t: print('Its work!\n')
+
+points = [{'x': 2, 'y': 3}, {'x': 4, 'y': 1}]
+points.sort(key=lambda i: i['y'])
+print(points)
+
+listOne = [2, 3, 4]
+listTwo = [2*i for i in listOne if i > 2]
+print(listTwo)
