151e798b4bca3dd98221fb23c67783ca37d27a57
ArtemPD < >
2019-05-20 18:09:56 +0300

    some 3

--- APerehon/Homework/Homework.oop.py
+++ APerehon/Homework/Homework.oop.py
@@ -1,27 +1,4 @@
-class Hero:
-    '''Class to Create Hero for our Game'''
-    def __init__(self, name, level, race):
-        '''Initiate our hero'''
-        self.name = name
-        self.level = level
-        self.race = race
-        self.health = 100
-    def show_hero(self):
-        """print all parameters of this Hero"""
-        discription = (self.name + " Level is: " + str(self.level) + " Race is: "+ self.race + " Health is: "+ str(self.health)).title()
-        print(discription)
-
-    def level_up(self):
-        """Upgrade Level of Hero"""
-        self.level += 1
-
-    def move(self):
-        """ Start moving hero"""
-        print("Hero " + self.name + " start moving...")
-    def set_health(self, new_health):
-        self.health = new_health
-
-
+from hero import *
 #------------MAIN---------------------
 
 myhero1 = Hero("Vurdalak", 5, "Orc")
@@ -33,6 +10,14 @@ myhero1.level_up()
 myhero1.show_hero()
 myhero1.set_health(60)
 myhero1.show_hero()
+
+myhero = Hero("Vurdalak", 4, "Orc")
+mysuperhero = SuperHero("Moisey", 10, "Human", 5)
+
+myhero.show_hero()
+mysuperhero.showhero()
+
+
 
 
 
