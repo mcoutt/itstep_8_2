8223a8e8a808d39cf425e2b02800ec49a3ea4f9a
ArtemPD < >
2019-05-20 17:37:41 +0300

    some 3

--- APerehon/Homework/Homework.oop.py
+++ APerehon/Homework/Homework.oop.py
@@ -8,12 +8,32 @@ class Hero:
         self.health = 100
     def show_hero(self):
         """print all parameters of this Hero"""
-       discription = (self.name + "Level is:  " + str(self.level) + "Race is: "+ self.race + "Health is: "+ str(self.health)).title()
-       print(discription)
+        discription = (self.name + " Level is: " + str(self.level) + " Race is: "+ self.race + " Health is: "+ str(self.health)).title()
+        print(discription)
 
     def level_up(self):
         """Upgrade Level of Hero"""
-        self.level = 
+        self.level += 1
+
+    def move(self):
+        """ Start moving hero"""
+        print("Hero " + self.name + " start moving...")
+    def set_health(self, new_health):
+        self.health = new_health
+
+
+#------------MAIN---------------------
+
+myhero1 = Hero("Vurdalak", 5, "Orc")
+myhero2 = Hero("Alexander", 4, "Human")
+
+myhero1.show_hero()
+myhero2.move()
+myhero1.level_up()
+myhero1.show_hero()
+myhero1.set_health(60)
+myhero1.show_hero()
+
 
 
 
