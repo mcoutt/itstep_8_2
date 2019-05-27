ba76bdebe3919d65c56a6fb8868af80bfd29c638
illuhich <1234papricka@gmail.com>
2019-05-27 21:45:12 +0300

    requests

---
+++ redkolis/123.py
@@ -0,0 +1,14 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+for voice in voices:
+    print("Voice:")
+    print(" - ID: %s" % voice.id)
+    print(" - Name: %s" % voice.name)
+    print(" - Languages: %s" % voice.languages)
+    print(" - Gender: %s" % voice.gender)
+    print(" - Age: %s" % voice.age)
+<div data-qa="vacancy-serp__vacancy_snippet_responsibility"><resp></resp></div>
+<div data-qa="vacancy-serp__vacancy_snippet_requirement">Basic algorithms and data structures. Good knowledge of <span class="highlighted highlighted_short">Python</span>. Shell scripting is a plus. Knowledge of C, PHP, Perl and...</div>
---
+++ redkolis/For_Human.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+headers = {'accept': '*/*',
+           'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
+
+base_url = 'https://kiev.hh.ua/search/vacancy?area=5&clusters=true&currency_code=UAH&enable_snippets=true&search_period=3&text=python&page=0'
+
+
+def hh_parse(base_url, headers):
+    jobs = []
+    session = requests.Session()
+    request = session.get(base_url, headers=headers)
+    if request.status_code == 200:
+        soup = bs(request.content, 'html.parser')
+        divs = soup.find_all('div', attrs={'data-qa': 'vacancy-serp__vacancy'})
+        for div in divs:
+            title = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'}).text
+            href = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'})['href']
+            company = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-employer'}).text
+            text2 = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_requirement'}).text
+            text1 = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_responsibility'}).text
+            content = text1 + ' ' + text2
+            jobs.append({
+                'title': title,
+                'href': href,
+                'company': company,
+                'content': content
+            })
+        print(len(jobs))
+
+    else:
+        print('ERROR')
+
+
+hh_parse(base_url, headers)
