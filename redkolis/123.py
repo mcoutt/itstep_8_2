8621dfc4e8e47308570ce425fa1bf47b38468b1d
illuhich <1234papricka@gmail.com>
2019-05-27 21:46:06 +0300

    requests

--- redkolis/123.py
+++ redkolis/123.py
@@ -10,5 +10,5 @@ for voice in voices:
     print(" - Languages: %s" % voice.languages)
     print(" - Gender: %s" % voice.gender)
     print(" - Age: %s" % voice.age)
-<div data-qa="vacancy-serp__vacancy_snippet_responsibility"><resp></resp></div>
-<div data-qa="vacancy-serp__vacancy_snippet_requirement">Basic algorithms and data structures. Good knowledge of <span class="highlighted highlighted_short">Python</span>. Shell scripting is a plus. Knowledge of C, PHP, Perl and...</div>
+# <div data-qa="vacancy-serp__vacancy_snippet_responsibility"><resp></resp></div>
+# <div data-qa="vacancy-serp__vacancy_snippet_requirement">Basic algorithms and data structures. Good knowledge of <span class="highlighted highlighted_short">Python</span>. Shell scripting is a plus. Knowledge of C, PHP, Perl and...</div>
