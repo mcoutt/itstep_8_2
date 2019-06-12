bd31829551f5b676d072890cfa05115d11606601
ArtemPD < >
2019-06-12 17:40:53 +0300

    some 3

---
+++ APerehon/Homework/Homework11.py
@@ -0,0 +1,32 @@
+def decorator(func):
+    def wrapper():
+        print("Код до выполнения функции")
+        func()
+        print("Код,который сработал после функции")
+    return wrapper
+
+
+def test(func):
+    def wrapper():
+        print("Код до выполнения функции 2")
+        func()
+        print("Код,который сработал после функции 2")
+    return wrapper
+
+
+@decorator
+@test
+def show():
+    print("Я обычная функция")
+
+
+
+#show = decorator(show)
+show()
+
+
+
+
+
+
+
