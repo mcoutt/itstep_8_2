bc76c48823a98e73d55a8685d8a745d6739c4fcb
student <lenusenko@gmail.com>
2019-06-19 21:49:47 +0300

    All is okey

--- APerehon/lessons/19.06.2019.py
+++ APerehon/lessons/19.06.2019.py
@@ -17,6 +17,68 @@ print(res)
 print(his)
 res = gen.send((4, 10, "g"))
 print(res)
-res, history = gen.send((999, 777,"history"))
+res, history = gen.send((999, 777, "history"))
 print(res)
 print(history)
+
+
+
+# def f():
+#     start = 0
+#     end = 5
+#     point = start
+#     while True:
+#         while point < end:
+#             yield point
+#             point += 1
+#         while point > start:
+#             yield point
+#             point -=1
+# import time
+# a = f()
+#
+# while True:
+#     print(next(a))
+#     time.sleep(0.3)
+
+
+
+
+def f():
+    start,end = yield
+    point = start
+    while True:
+        while point < end:
+            data =yield point
+            if data is not None:
+                start,end = data
+                point = start
+                continue
+            point += 1
+        while point > start:
+            data = yield point
+            if data is not None:
+                start,end = data
+                point = start
+                continue
+
+            point -=1
+import time
+import random as R
+gen = f()
+next(gen)
+print(gen.send((1,4)))
+while True:
+    a = R.randint(0,10)
+    if a == 3:
+        start,end = (map(int, input("->").split()))
+        point = gen.send((start,end))
+    else:
+        point = next(gen)
+    print(point)
+    time.sleep(0.3)
+
+
+
+
+
