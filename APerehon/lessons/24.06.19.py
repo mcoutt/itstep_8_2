14de64c21a04d2c7a2e39b15183863e7a196ef0b
student <lenusenko@gmail.com>
2019-06-24 20:35:47 +0300

    All is okey

---
+++ APerehon/lessons/24.06.19.py
@@ -0,0 +1,25 @@
+# def f():
+#     a = int(input())
+#     if a == 0:
+#         return 0
+#     else:
+#         f()
+#         print(a)
+#
+# f()
+#
+
+# import random as R
+# def f():
+#     a = R.randint(0,10000000)
+#     if a == 555:return 0
+#     else:
+#         f()
+#         print(a)
+#         return
+#
+#
+# f()
+
+
+
