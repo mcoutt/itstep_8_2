612a899fac701098156ba1e62dd745ba68c7c0bb
student <lenusenko@gmail.com>
2019-06-26 21:53:29 +0300

    All is okey

---
+++ APerehon/lessons/26.06.19.py
@@ -0,0 +1,14 @@
+# import msvcrt
+#
+# i = 0
+# while True:
+#     if msvcrt.kbhit():
+#         key = msvcrt.getch().decode()
+#         if key == "j":
+#             a = input("-->")
+#             print(i)
+#     i += 1
+
+def f(a:int,c:int)->str:
+    return str(a)+str(c)
+
