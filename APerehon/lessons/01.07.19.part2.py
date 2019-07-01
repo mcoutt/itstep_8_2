2d3fcf9d2b6685aa66ad7f5532559306ed90580a
student <lenusenko@gmail.com>
2019-07-01 20:40:18 +0300

    All is okey

---
+++ APerehon/lessons/01.07.19.part2.py
@@ -0,0 +1,19 @@
+import socket
+import time
+s = socket.socket()
+s.connect(('localhost',5000))
+
+data = input("Send>")
+s.sendall(data.encode())
+
+while True:
+    try:
+        data = s.recv(1024).decode()
+    except:
+        print('Server dead!!!')
+        break
+    print(data)
+    data = input("Send>")
+    s.sendall(data.encode())
+
+s.close()
