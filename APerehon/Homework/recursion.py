8eb0b5f28ee66e2df7a9a3a6fbee1a1616331a19
ArtemPD < >
2019-08-11 20:58:27 +0300

    recursion

--- APerehon/Homework/recursion.py
+++ APerehon/Homework/recursion.py
@@ -8,11 +8,41 @@
 # rec(1)
 
 
-def fact(x):
-    if x == 1:
-        return 1
-    return fact(x-1)*x
+# def fact(x):
+#     if x == 1:
+#         return 1
+#     return fact(x-1)*x
+#
+#
+# print(fact(4))
+# print(fact(10))
+
+# Fibonacci
+
+# def fib(n):
+#     if n == 1:
+#         return 0
+#     elif n == 2:
+#         return 1
+#     return fib(n-1)+fib(n-2)
+#
+#
+# print(fib(5))
+# print(fib(6))
+# print(fib(7))
+
+# палиндром
+# шалаш
+# asdffdsa
+#'' 'a'
 
 
-print(fact(4))
-print(fact(10))
+def palindrom(s):
+    if len(s) <= 1:
+        return True
+    elif s[0] != s[-1]:
+        return False
+    return palindrom(s[1:-1])
+
+
+print(palindrom('фф'))
